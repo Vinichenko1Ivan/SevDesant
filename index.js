f6ad3dd149e45f3976154a5d35454c9e5c55c941
Vinichenko1Ivan <115999423+Vinichenko1Ivan@users.noreply.github.com>
2024-01-27 01:21:21 +0500

    burger-menu (v 0.1) done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+document.querySelector('.header__burger-menu').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.burger-menu__list').classList.toggle('drop-menu');
+})
+
+/*document.querySelector('.header__burger-menu').addEventListener('click', function () {
+    this.classList.toggle('deactive');
+    document.querySelector('.burger-menu__list').classList.toggle('pick-up-menu');
+})*/
